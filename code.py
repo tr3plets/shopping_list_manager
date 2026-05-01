b0b8cce5754b1ab328e0418f1f6af106595f4604
Zhang <hiligan.jonahmae0o14@gmail.com>
2026-05-01 14:34:03 +0000

    student C

--- code.py
+++ code.py
@@ -1,9 +1,53 @@
 try:
     with open("shopping_list.txt", "x") as f:
-        f.write("Created new file successfully.")
+        f.write("Created new file successfully.\n")
         
 except FileExistsError:
     print("File already exists.")
 
-    with open("shopping_list.txt", "w") as f:
-        f.write("Overwritten existing file.")
+    with open("shopping_list.txt", "a") as f:
+        f.write("Program opened.\n")
+        
+        while True:
+            
+            try:
+                print("Menu")
+                print("1. Add an item.")
+                print("2. Change an item.")
+                print("3. Remove an item.")    
+                print("R. Read file and count lines.")
+                print("4. Exit.")
+
+                choice = input("Enter a choice: ")
+            
+            except ValueError:
+                print("Enter a valid choice.")
+            
+            break
+    
+    while True:
+        if choice == "1":
+            itemlist = input("Enter an item: ")
+            
+            with open("shopping_list.txt", "a") as f:
+                f.write(itemlist + "\n")
+                
+            print("Item added.")
+            break
+
+        elif choice.upper() == "R":
+            with open("shopping_list.txt", "r") as f:
+                lines = f.readlines()
+
+                print("File Contents:")
+                
+                for line in lines:
+                    print(line.strip())
+
+                print("Line Count:", len(lines))
+                
+            break
+
+        elif choice == "4":
+            print("Program ended.")
+            break
