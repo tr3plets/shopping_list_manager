6218d020b26a60ad24e3ab224b3e456c0bcb4c80
Sam Adolph Lim <officialsamacc@gmail.com>
2026-05-01 14:48:14 +0000

    update, search, delete

--- code.py
+++ code.py
@@ -17,6 +17,7 @@ except FileExistsError:
                 print("3. Remove an item.")    
                 print("R. Read file and count lines.")
                 print("4. Exit.")
+                print("5. Search item.")
 
                 choice = input("Enter a choice: ")
             
@@ -46,6 +47,55 @@ except FileExistsError:
 
                 print("Line Count:", len(lines))
                 
+            break
+
+        elif choice == "2":
+            old_item = input("Enter item to change: ")
+            new_item = input("Enter new item: ")
+
+            with open("shopping_list.txt", "r") as f:
+                data = f.readlines()
+
+            with open("shopping_list.txt", "w") as f:
+                for line in data:
+                    if line.strip() == old_item:
+                        f.write(new_item + "\n")
+                    else:
+                        f.write(line)
+
+            print("Item updated.")
+            break
+
+        elif choice == "3":
+            delete_item = input("Enter item to delete: ")
+
+            with open("shopping_list.txt", "r") as f:
+                data = f.readlines()
+
+            with open("shopping_list.txt", "w") as f:
+                for line in data:
+                    if line.strip() != delete_item:
+                        f.write(line)
+
+            print("Item deleted.")
+            break
+
+        elif choice == "5":
+            search_item = input("Enter item to search: ")
+
+            with open("shopping_list.txt", "r") as f:
+                data = f.readlines()
+
+            found = False
+
+            for line in data:
+                if search_item.lower() in line.lower():
+                    print("Item found:", line.strip())
+                    found = True
+
+            if found == False:
+                print("Item not found.")
+
             break
 
         elif choice == "4":
